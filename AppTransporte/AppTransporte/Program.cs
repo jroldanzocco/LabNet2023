5a2f41d01cbd4c062b720a1839ecf39b8add1820
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2023-08-16 21:04:33 -0300

    Creacion de clases
    Se crearon las clases correspondientes segun UML

---
+++ AppTransporte/AppTransporte/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTransporte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ AppTransporte/AppTransporte/TransportePublico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTransporte
+{
+    public abstract class TransportePublico
+    {
+        public int Pasajeros { get; }
+        public abstract void Avanzar();
+        public abstract void Detenerse();
+
+    }
+}
