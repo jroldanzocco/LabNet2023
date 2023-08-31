e141c765752264ff765eb1b5e8ba0b3c37695505
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2023-08-30 21:48:39 -0300

    Creacion de solucion, capas y EF

---
+++ Northwind.To.EF/Northwind.To.EF.Logic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.To.EF.Logic
+{
+    public class Class1
+    {
+    }
+}
