10fdcc3cd33605b94492b117d41939f844a8229a
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2023-09-03 21:31:59 -0300

    Creacion clase para delvolver join

---
+++ LINQ.Practica/LINQ.Practica.Entities/CustomerOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ.Practica.Entities
+{
+    class CustomerOrder
+    {
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string Region { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
