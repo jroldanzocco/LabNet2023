eaa6fc220d2e715baa12b6aeb01036d2cf9cc9f3
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2023-09-06 10:24:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-let $boton = document.getElementById("boton-enviar");
+let $boton = document.getElementById("enviar-numero");
+let $alerta = document.getElementById("alerta");
+let $ingresoNumero = document.getElementById("ingreso-numero");
+let $resultado = document.getElementById("resultado");
 
-$boton.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log(getRandomNumber());
+const numeroSecreto = getRandomNumber();
+let vidas;
+let puntaje = 5;
+let highscore = 0;
+
+function ReiniciarJuego() {
+  puntaje = 0;
+  vidas = 3;
+}
+console.log(numeroSecreto);
+ReiniciarJuego();
+$boton.addEventListener("click", function () {
+  if ($ingresoNumero.value == numeroSecreto) {
+    puntaje++;
+  } else {
+    vidas--;
+  }
+  console.log("vidas: " + vidas);
+  console.log("Puntaje: " + puntaje);
 });
 
 function getRandomNumber() {
   return Math.floor(Math.random() * 20 + 1);
 }
+
+$ingresoNumero.addEventListener("input", function () {
+  this.value = this.value.replace(/[eE]/g, "");
+  if (this.value < 1) {
+    this.value = "";
+  } else if (this.value > 20) {
+    this.value = parseInt(this.value / 10);
+  }
+});
