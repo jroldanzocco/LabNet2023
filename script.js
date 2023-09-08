552203896610d9ea085a79f7087bec74928680db
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2023-09-07 22:40:42 -0300

    Finalizada logica
    Se agrego logica separando por un lado los eventos tanto de input como de click y por otro las funciones

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 let $boton = document.getElementById("enviar-numero");
 let $ingresoNumero = document.getElementById("ingreso-numero");
+let $puntaje = document.getElementById("puntaje");
+let $highscore = document.getElementById("highscore");
+let $pista = document.getElementById("pista");
+let $ronda = document.getElementById("ronda");
 
-const numeroSecreto = getRandomNumber();
-let vidas;
-let puntaje = 5;
+let numeroSecreto;
+let puntaje;
 let highscore = 0;
+let ronda = 1;
 
-function ReiniciarJuego() {
-  puntaje = 0;
-  vidas = 3;
-}
-console.log(numeroSecreto);
-ReiniciarJuego();
+iniciarJuego(ronda);
+
+//Eventos
 $boton.addEventListener("click", function () {
   if ($ingresoNumero.value == numeroSecreto) {
-    puntaje++;
-  } else {
-    vidas--;
+    puntaje += 3;
+    ronda++;
+    getHighscore(puntaje);
+    victoria();
+  } else if (
+    $ingresoNumero.value != numeroSecreto &&
+    $ingresoNumero.value != ""
+  ) {
+    puntaje--;
+    pista();
   }
-  console.log("vidas: " + vidas);
-  console.log("Puntaje: " + puntaje);
-});
 
-function getRandomNumber() {
-  return Math.floor(Math.random() * 20 + 1);
-}
+  if (puntaje == 0) {
+    derrota();
+  }
+  $puntaje.innerText = puntaje;
+});
 
 $ingresoNumero.addEventListener("input", function () {
   this.value = this.value.replace(/[eE]/g, "");
@@ -34,3 +41,57 @@ $ingresoNumero.addEventListener("input", function () {
     this.value = parseInt(this.value / 10);
   }
 });
+
+//Funciones
+const getHighscore = (puntos) => {
+  if (puntos > highscore) {
+    highscore = puntos;
+  }
+  return highscore;
+};
+
+function victoria() {
+  Swal.fire({
+    icon: "success",
+    title: "Ganaste",
+    text: `Bien ahi! Adivinaste el numero secreto. Era ${numeroSecreto}`,
+  });
+  iniciarJuego(ronda);
+}
+function derrota() {
+  Swal.fire({
+    icon: "error",
+    title: "Perdiste",
+    text: `Mala suerte! El numero secreto era ${numeroSecreto}`,
+  });
+  ronda = 1;
+  iniciarJuego(ronda);
+}
+
+function iniciarJuego(ronda) {
+  $pista.innerText = "Adivina el número entre 1 y 20";
+  $ingresoNumero.value = "";
+  reiniciarNumero();
+  if (ronda == 1) {
+    puntaje = 20;
+  }
+  $ronda.innerText = `Ronda: ${ronda}`;
+  $puntaje.innerText = puntaje;
+  $highscore.innerText = highscore;
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 20 + 1);
+}
+
+function reiniciarNumero() {
+  numeroSecreto = getRandomNumber();
+}
+
+function pista() {
+  if ($ingresoNumero.value < numeroSecreto) {
+    $pista.innerText = "Muy bajo!";
+  } else if ($ingresoNumero.value > numeroSecreto) {
+    $pista.innerText = "Muy alto!";
+  }
+}
