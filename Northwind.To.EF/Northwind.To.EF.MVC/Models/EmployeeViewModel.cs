6dc47c87971d802c61bd73e3a78aa033771ef23f
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2023-09-10 19:59:11 -0300

    Correccion dataAnottation

--- Northwind.To.EF/Northwind.To.EF.MVC/Models/EmployeeViewModel.cs
+++ Northwind.To.EF/Northwind.To.EF.MVC/Models/EmployeeViewModel.cs
@@ -10,15 +10,18 @@ namespace Northwind.To.EF.MVC.Models
     {
         [Required]
         [StringLength(10)]
-        [RegularExpression(@"^[A-Za-záéíóúÁÉÍÓÚñÑ]+$", ErrorMessage = "El nombre solo puede contener letras y acentos.")]
-        [Display()]
+        [RegularExpression(@"^[A-Za-záéíóúÁÉÍÓÚñÑ]+$")]
+        [Display(Name = "Nombre")]
         public string Nombre { get; set; }
         [Required]
-        [StringLength(10)]
-        [RegularExpression(@"^[A-Za-záéíóúÁÉÍÓÚñÑ]+$", ErrorMessage = "El nombre solo puede contener letras y acentos.")]
+        [StringLength(20)]
+        [RegularExpression(@"^[A-Za-záéíóúÁÉÍÓÚñÑ]+$")]
+        [Display(Name = "Apellido")]
         public string Apellido { get; set; }
         [Required]
-        [StringLength(10)]
+        [StringLength(30)]
+        [RegularExpression(@"^[A-Za-záéíóúÁÉÍÓÚñÑ]+$")]
+        [Display(Name = "Rol")]
         public string Rol { get; set; }
     }
 }
