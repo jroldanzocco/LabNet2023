51e2ea691f22836e04348b7f789d5d5bd1d5f3bc
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2023-09-20 09:24:06 -0300

    Validacion back minimo caracter

--- Northwind.To.EF/Northwind.To.EF.WebAPI/Models/Employee.cs
+++ Northwind.To.EF/Northwind.To.EF.WebAPI/Models/Employee.cs
@@ -12,14 +12,17 @@ namespace Northwind.To.EF.WebAPI.Models
         [Required]
         [RegularExpression(@"^[a-zA-Z]+( [a-zA-Z]+)*$", ErrorMessage = "Solo se permite letras")]
         [StringLength(10, ErrorMessage = "Maximo 10 caracteres")]
+        [MinLength(2, ErrorMessage = "Minimo dos caracteres.")]
         public string FirstName { get; set; }
         [Required]
         [RegularExpression(@"^[a-zA-Z]+( [a-zA-Z]+)*$", ErrorMessage = "Solo se permite letras")]
         [StringLength(20, ErrorMessage = "Maximo 20 caracteres")]
+        [MinLength(2, ErrorMessage = "Minimo dos caracteres.")]
         public string LastName { get; set; }
         [Required]
         [RegularExpression(@"^[a-zA-Z]+( [a-zA-Z]+)*$", ErrorMessage = "Solo se permite letras")]
         [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        [MinLength(2, ErrorMessage = "Minimo dos caracteres.")]
         public string Title { get; set; }
     }
 }
